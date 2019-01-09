1301a067427052f22248a4b513810a0298f50f80
Łukasz Król <lionel@o2.pl>
2019-01-09 22:16:19 +0100

    Add get quote and create tweet functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var url = "http://api.icndb.com/jokes/random";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var tweetLink = "https://twitter.com/intent/tweet?text=";
 var paragraph = document.getElementById("joke");
 var button = document.getElementById("random-joke");
 
@@ -8,14 +9,28 @@ button.addEventListener("click", function() {
   getJoke();
 });
 
-function getJoke() {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", url);
-  xhr.addEventListener("load", function() {
-    var response = JSON.parse(xhr.response);
-    paragraph.innerHTML = response.value.joke;
-  });
-  xhr.send();
-};
+// Get quote function
+function getQuote() {
+  fetch(quoteUrl, {cache: "no-store"})
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(createTweet);
+}
+
+// Create tweet
+function createTweet(input) {
+  var data = input[0];
+  var dataElement = document.createElement("div");
+  dataElement.innerHTML = data.content;
+  var quoteText = dataElement.innerText.trim();
+  var quoteAuthor = data.title;
+
+  if (!quoteAuthor.length) {
+    quoteAuthor = "Unknow author";
+  }
+
+  var tweetText = "Quote of the day - " + quoteText + ". Author: " + quoteAuthor;
+}
 
 window.onload = getJoke();
